da17db56dfd4a254d24b014ca58822f42a9fa56e
Auevet77 <104871346+Auevet77@users.noreply.github.com>
2022-09-20 19:37:16 -0300

    Página Login Ok

--- Miamo.UI/Pages/Login.aspx.cs
+++ Miamo.UI/Pages/Login.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Miamo.BLL;
+using Miamo.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,48 @@ namespace Miamo.UI.Pages
 {
     public partial class Login : System.Web.UI.Page
     {
+        //limpar
+        public void Limpar()
+        {
+            txtUser.Text = txtPassword.Text = string.Empty;
+            txtUser.Focus();
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Page.IsPostBack)
+            {
 
+            }
+        }
+
+        protected void btnEntrar_Click(object sender, EventArgs e)
+        {
+            string objNome = txtUser.Text.Trim();
+            string objSenha = txtPassword.Text.Trim();
+
+            UsuarioAutenticaDTO objModelo = new UsuarioAutenticaDTO();
+            UsuarioBLL objAutentica = new UsuarioBLL();
+            objModelo = objAutentica.AutenticarUser(objNome, objSenha);
+            if (objModelo != null && objModelo.FKTpUsuario == "1")
+            {
+                Session["Usuario"] = txtUser.Text.Trim();
+                Response.Redirect("GerenciarUsuario.aspx");
+            }
+            else if (objModelo != null && objModelo.FKTpUsuario == "2")
+            {
+                lblMensagem.Text = "Você não tem privilégios para esta operação !!!";
+                Limpar();
+            }
+            else
+            {
+                lblMensagem.Text = "Usuario não cadastrado !!!";
+                Limpar();
+            }
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Limpar();
         }
     }
 }
