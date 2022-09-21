379376bcbfe5540d410aeb9cc719e9f3e12f5bfb
Auevet77 <104871346+Auevet77@users.noreply.github.com>
2022-09-20 22:53:59 -0300

    Criação do Miamo.SITE

---
+++ Miamo.SITE/js/trocaimagem.js
@@ -0,0 +1,49 @@
+//Lingeries
+
+function lingieri10(){
+    document.getElementById("lingerieAzulMarinho01").src="img/imgCatalogo/Lingieries/10.jpg";
+}
+
+function lingieri09(){
+    document.getElementById("lingerieAzulMarinho01").src="img/imgCatalogo/Lingieries/09.jpg";
+}
+
+function lingieri04(){
+    document.getElementById("lingeriePreta01").src="img/imgCatalogo/Lingieries/04.jpg";
+}
+
+function lingieri02(){
+    document.getElementById("lingeriePreta01").src="img/imgCatalogo/Lingieries/02.jpg";
+}
+
+function lingieri11(){
+    document.getElementById("lingerieAzulMarinho02").src="img/imgCatalogo/Lingieries/11.jpg";
+}
+
+function lingieri12(){
+    document.getElementById("lingerieAzulMarinho02").src="img/imgCatalogo/Lingieries/12.jpg";
+}
+
+function lingieri18(){
+    document.getElementById("lingeriePreta02").src="img/imgCatalogo/Lingieries/18.jpg";
+}
+
+function lingieri19(){
+    document.getElementById("lingeriePreta02").src="img/imgCatalogo/Lingieries/19.jpg";
+}
+
+function lingieri16(){
+    document.getElementById("lingerieAzulEscuro01").src="img/imgCatalogo/Lingieries/16.jpg";
+}
+
+function lingieri17(){
+    document.getElementById("lingerieAzulEscuro01").src="img/imgCatalogo/Lingieries/17.jpg";
+}
+
+function lingieri15(){
+    document.getElementById("lingerieAzulEscuro02").src="img/imgCatalogo/Lingieries/15.jpg";
+}
+
+function lingieri14(){
+    document.getElementById("lingerieAzulEscuro02").src="img/imgCatalogo/Lingieries/14.jpg";
+}
